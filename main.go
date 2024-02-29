ebb698ffa384f11f1cb8753654657bc23cb26220
Mchl37 <71225509+Mchl37@users.noreply.github.com>
2024-02-29 10:53:49 +0100

    Front-end created

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+	http.ServeFile(w, r, "./public/index.html")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -19,15 +22,36 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	r := chi.NewRouter()
 
-	err := http.ListenAndServe(":1234", nil)
+	// Middleware
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
+	// Servir les fichiers statiques
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(workDir + "/public")
+	FileServer(r, "/assets", filesDir)
+
+	// Routes
+	r.Get("/", getRoot)
+	r.Get("/hello", getHello)
+
+	// Démarrer le serveur
+	err := http.ListenAndServe(":1234", r)
+	if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+// FileServer crée un gestionnaire de fichiers statiques
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer ne prend pas de caractères de substitution ou de pièces globales.")
+	}
+	fs := http.StripPrefix(path, http.FileServer(root))
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
