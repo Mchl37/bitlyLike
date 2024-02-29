5d49d439547cd315a20cae9baed70cd26cc3c831
Mchl37 <71225509+Mchl37@users.noreply.github.com>
2024-02-29 10:07:49 +0100

    Web server added

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
-func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is running on http://localhost:1234/")
-	http.ListenAndServe(":8080", nil)
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bienvenue sur BitlyLike !")
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":1234", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
